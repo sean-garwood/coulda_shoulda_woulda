866cd3a885f9a638a28574326ca4cf9ddd3e517d
sean-garwood <seansgarwood@gmail.com>
2026-02-21 14:15:48 -0500

    init

---
+++ lib/coulda_shoulda_woulda.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "coulda_shoulda_woulda/version"
+
+module CouldaShouldaWoulda
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/coulda_shoulda_woulda/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CouldaShouldaWoulda
+  VERSION = "0.1.0"
+end
